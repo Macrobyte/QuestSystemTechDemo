9320fb1f9b0a950dc111d78fda01f49642555296
Vasco Almeida <l.vasco.m.a21@gmail.com>
2025-07-17 14:46:29 +0100

    Added project files

---
+++ Source/QuestSystemTechDemo/QuestSystemTechDemo.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class QuestSystemTechDemo : ModuleRules
+{
+	public QuestSystemTechDemo(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
